95485387b2ebf1f75f861001c298d356b5f916c0
Tyler <Tyler@TYLER-PC>
2017-04-04 20:46:32 -0500

    Updated register

---
+++ cs341_project/cs341_project/Controllers/AddUserController.cs
@@ -0,0 +1,28 @@
+﻿using cs341_project.Facades;
+using cs341_project.Models;
+using System.Web.Http;
+
+namespace cs341_project.Controllers
+{
+    public class AddUserController : ApiController
+    {
+        [Route("api/AddAppointment")]
+        [HttpPost]
+        public string Post(Register app)
+        {
+            User temp = new Models.User();
+            temp.FirstName = app.FirstName;
+            temp.LastName = app.LastName;
+            temp.Username = app.Username;
+            temp.Password = app.Password;
+            temp.Type = app.Type;
+            DatabaseFacade facade = new DatabaseFacade();
+            if (true)
+            {
+                return "Username " + app.Username + " already exists ";
+            }
+            return "ERROR";
+
+        }
+    }
+}
--- cs341_project/cs341_project/Scripts/TylerScripts/registerController.js
+++ cs341_project/cs341_project/Scripts/TylerScripts/registerController.js
@@ -1,5 +1,28 @@
 ﻿cs341.controller('registerController', function ($scope) {
+    scope = $scope;
 
-    $scope.message = 'register';
+    scope.adminKey = "admin";
+    scope.employeeKey = "employ";
+
+    scope.register = function () {
+        scope.input = {
+            FirstName: scope.firstName,
+            LastName: scope.lastName,
+            Username: scope.username,
+            Password: scope.password,
+            Role: null,
+            Type: null
+        }
+
+        if (scope.key === scope.adminKey) {
+            scope.input.Type = "admin";
+        }
+
+        if (scope.key === scope.employeeKey) {
+            scope.input.Type = "employee";
+            scope.input.Role = scope.employee;
+            alert(scope.input.Role + " ");
+        }
+    }
 
 });
